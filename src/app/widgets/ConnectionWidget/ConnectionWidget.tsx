c23536ff409fd0353bc9dfda5d8a6236e71c5fe7
Nikita Schastnyy <smynay@yandex.ru>
2024-06-15 17:44:09 +0300

    move connection widget to typography widget

--- src/app/widgets/ConnectionWidget/ConnectionWidget.tsx
+++ src/app/widgets/ConnectionWidget/ConnectionWidget.tsx
@@ -1,15 +1,16 @@
 import { useViewModel } from "../../providers";
 import { observer } from "mobx-react-lite";
+import { Typography } from "../../components";
 export const ConnectionWidget = observer(function ConnectionWidget() {
   const connectionVM = useViewModel("connectionWidgetViewModel");
 
   if (connectionVM.isLoading) {
-    return <p>Connecting...</p>;
+    return <Typography>Connecting...</Typography>;
   }
 
   if (!connectionVM.isLoading && !connectionVM.isConnected) {
-    return <p onClick={connectionVM.connect}>Disconnected</p>;
+    return <Typography onClick={connectionVM.connect}>Disconnected</Typography>;
   }
 
-  return <p onClick={connectionVM.disconnect}>Connected</p>;
+  return <Typography onClick={connectionVM.disconnect}>Connected</Typography>;
 });
