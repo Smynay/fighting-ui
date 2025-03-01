236b84558ca83a98096f62bfbaa32d38bed3141f
Nikita Schastnyy <smynay@yandex.ru>
2025-03-01 14:34:43 +0300

    fix legacy ws connection

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,12 +3,13 @@ import reducer from "./features";
 import { socketMiddleware } from "./middlewares";
 import { connect, init, refuse, error, message } from "./features/connection";
 import { setRequest } from "./features/fighting";
+import { getServerUrl } from "../core/utils";
 
 export const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
-      socketMiddleware("ws://91.108.25.189:3000", {
+      socketMiddleware(getServerUrl(), {
         onInit: init.type,
         onConnect: connect.type,
         onClose: refuse.type,
