db6ae87343ee8c93e580e683afc3be8fd60507ca
Nikita Schastnyy <smynay@yandex.ru>
2025-04-02 15:32:48 +0300

    disable legacy

--- src/app/pages/MenuPage.tsx
+++ src/app/pages/MenuPage.tsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 export const MenuPage = () => {
   return (
     <div>
-      <Link to={"fighting"}>Fighting</Link>
-      <br />
-      <Link to={"legacy"}>Fighting (legacy)</Link>
+      <Link to={"fighting"}>Fighting</Link>{}>
+      {/* Last server version for legacy support at commit 236b8455*/}
+      {/*<br />*/}
+      {/*<Link to={"legacy"}>Fighting (legacy)</Link>*/}
     </div>
   );
 };
